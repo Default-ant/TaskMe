c214ead42a32a48c1ea15c7662628af230804cc9
Default-ant <krishnamani468@gmail.com>
2025-11-03 10:10:14 +0000

    new apislice for token auth

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -2,22 +2,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 const API_URI = import.meta.env.VITE_APP_BASE_URL
 
-// Updated baseQuery
 const baseQuery = fetchBaseQuery({ 
   baseUrl: API_URI + "/api",
   
-  // This is the part you need to add
-  prepareHeaders: (headers, { getState }) => {
-    // Get the token from your Redux state
-    // Adjust 'auth.token' to match your auth slice
-    const token = getState()?.auth?.user?.token 
-
-    if (token) {
-      headers.set('authorization', `Bearer ${token}`)
-    }
-    
-    return headers
-  },
+  // 👇 ADD THIS LINE 👇
+  credentials: 'include',
+  
+  // 👆 (And remove the 'prepareHeaders' function) 👆
 })
 
 export const apiSlice = createApi({
